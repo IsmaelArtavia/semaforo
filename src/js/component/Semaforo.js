79a4a4408bda26f55425bf933bc14ed5186c0d4c
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-08 04:05:01 +0000

    first commit

---
+++ src/js/component/Semaforo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Semaforo = () => {
+	return (
+		<div className="jumbotron jumbotron-fluid jumbotron--dark bg-dark ">
+			<div className="container"></div>
+		</div>
+	);
+};
+
+export default Semaforo;
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,15 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+import Semaforo from "./component/Semaforo.js";
+import Cable from "./component/cable.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+	<div>
+		<Home />
+		<Cable />
+		<Semaforo />
+	</div>,
+	document.querySelector("#app")
+);
